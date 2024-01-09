0c4f9420fdc1e6697c66ac0eb5ee1b0eab7ac47b
MrWhiteres <alexandr.leluk.99@gmail.com>
2024-01-09 20:42:05 +0200

    Add task solutions

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,24 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function(callback) {
+    const typeSort = callback
+      ? (index1, index2) => callback(this[index1], this[index2]) > 0
+      : (index1, index2) => `${this[index1]}` > `${this[index2]}`;
+
+    const changerFunc = (index1, index2) => {
+      [this[index1], this[index2]] = [this[index2], this[index1]];
+    };
+
+    for (let i = 0; i < this.length - 1; i++) {
+      for (let j = 0; j < this.length - i - 1; j++) {
+        if (typeSort(j, j + 1)) {
+          changerFunc(j, j + 1);
+        }
+      }
+    }
+
+    return this;
   };
 }
 
